153f319a09ad286589b3878106c28203dd5e22a9
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-02-22 23:50:22 +0300

    add fetching the tasks

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,13 +1,27 @@
+/* eslint-disable functional/no-expression-statements */
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 import TaskItem from "./TaskItem.jsx";
-import { useSelector } from "react-redux";
+import { fetchTasks } from "../redux/taskSlice.js";
 
 const TaskList = () => {
+  const dispatch = useDispatch();
+
   const tasks = useSelector((state) => state.tasks);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
 
   return (
     <ul className="list-group">
       {tasks.map((task) => (
-        <TaskItem id={task.id} title={task.title} completed={task.completed} />
+        <TaskItem 
+        key={task.id}
+        id={task.id} 
+        title={task.title} 
+        completed={task.completed} 
+    />
       ))}
     </ul>
   );
