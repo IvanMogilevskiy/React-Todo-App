a8c84cebfe583eef45c7d51240ca8886f54c4b3f
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-02-28 18:05:15 +0300

    add all components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css';
-import React from 'react';
+import './index.css';
+import { Container } from 'react-bootstrap';
+import AddTaskForm from './components/AddTaskForm.jsx';
+import TaskList from './components/TaskList.jsx';
+import CompletedTasks from './components/CompletedTasks.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      Work
-    </div>
+const App = () => (
+    <Container className="bg-white p-4 mt-5">
+      <h1>Things I need to do</h1>
+      <AddTaskForm />
+      <TaskList />
+      <CompletedTasks />
+    </Container>
   );
-}
 
 export default App;
