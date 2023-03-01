ca58181835237e0692323f11264ef97c540458ad
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-03-01 18:47:46 +0300

    add addTaskAsync and bootstrap elements

--- client/src/components/AddTaskForm.jsx
+++ client/src/components/AddTaskForm.jsx
@@ -1,7 +1,9 @@
+/* eslint-disable functional/no-conditional-statements */
 /* eslint-disable functional/no-expression-statements */
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addNewTask } from '../redux/taskSlice.js';
+import { Form, FloatingLabel, Button  } from 'react-bootstrap';
+import { addTaskAsync } from '../redux/taskSlice.js';
 
 
 const AddTaskForm = () => {
@@ -11,26 +13,30 @@ const AddTaskForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    dispatch(addNewTask({
-        title: value,
-      })
-    )
+    if (value) {
+      dispatch(addTaskAsync({
+          title: value,
+        })
+      )
+    }
   };
 
   return (
-    <form onSubmit={onSubmit} className="form-inline mt-3 mb-2">
-      <label className="sr-only">Name</label>
-      <input
-        type="text"
-        className="form-control mb-2 mr-sm-2"
-        placeholder="Add Task"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-       />
-      <button type="submit" className="btn btn-primary mb-2">
+    <Form onSubmit={onSubmit} className="form-inline mt-3 mb-2">
+      <FloatingLabel controlId="taskName" className="sr-only" label="Name">
+        <Form.Control
+          name="taskName"
+          type="text"
+          className="form-control mb-2 mr-sm-2"
+          placeholder="Add Task"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </FloatingLabel>
+      <Button type="submit" className="mb-2" variant="primary">
         Submit
-      </button>
-    </form>
+      </Button>
+    </Form>
   );
 };
 
