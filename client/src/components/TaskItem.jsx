4bd09b4066529760e6ce812fb3e8cef336aa1e41
Ivan Mogilevskiy <ivanmogilevskiy@gmail.com>
2023-03-01 18:49:02 +0300

    add completeTaskAsync, deleteTaskAsync, bootstrap

--- client/src/components/TaskItem.jsx
+++ client/src/components/TaskItem.jsx
@@ -1,25 +1,26 @@
 /* eslint-disable functional/no-expression-statements */
 import { useDispatch } from 'react-redux';
-import { completeTask, deleteTask } from '../redux/taskSlice.js';
+import { ListGroupItem } from 'react-bootstrap';
+import { completeTaskAsync, deleteTaskAsync } from '../redux/taskSlice.js';
 
 const TaskItem = ({ id, title, completed }) => {
   const dispatch = useDispatch();
 
   const handleCompleteTask = () => {
-    dispatch(completeTask({ id, completed: !completed}));
+    dispatch(completeTaskAsync({ id, completed: !completed}));
   };
 
   const handleDeleteTask = () => {
-    dispatch(deleteTask({ id }));
+    dispatch(deleteTaskAsync({ id }));
   };
 
   return (
-    <li className={`list-group-item ${completed && "list-group-item-success"}`}>
+    <ListGroupItem className={`${completed && "list-group-item-success"}`}>
       <div className="d-flex justify-content-between">
         <span className="d-flex align-items-center">
           <input 
             type="checkbox" 
-            className="mr-3" 
+            className="me-3" 
             checked={completed}
             onChange={handleCompleteTask}
           />
@@ -33,7 +34,7 @@ const TaskItem = ({ id, title, completed }) => {
           Delete
         </button>
       </div>
-    </li>
+    </ListGroupItem>
   );
 };
 export default TaskItem;
